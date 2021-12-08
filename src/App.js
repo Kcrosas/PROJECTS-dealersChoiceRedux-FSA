190f0415e773e9aa077769868c7a6da977a478d1
Kenneth Rosas <kencrosas@gmail.com>
2021-12-08 01:17:56 -0500

    Single state change component

--- src/App.js
+++ src/App.js
@@ -5,27 +5,44 @@ class App extends Component {
     constructor(){
         super();
         this.state = {
-            riders: []
+            riders: [],
+            selected: ''
         }
     }
     async componentDidMount(){
         const riders = (await axios.get('/riders')).data
         console.log(riders)
         this.setState({ riders })
+        window.addEventListener('hashchange', async() => {
+            const exactRider = window.location.hash.slice(1)
+            const selection = (await axios.get(`/riders/${exactRider}`)).data
+            this.setState({ selected: selection})
+            
+        })
     }
     render () {
-        const { riders } = this.state
+        const { riders, selected} = this.state
         return (
+            this.state.selected ?  
+            <div>
+                <h1>Rider: {selected.name}</h1>
+                <ul>
+                    <li>Bio: {selected.bio}</li>
+                    <li>Member date: {selected.memberDate}</li>
+                </ul>
+            </div>
+            
+            : 
+
             <ul>
-               {
-                   riders.map(e=>{
+               {riders.map(e=>{
                        return <li key = { e.id}>
-                           { e.name }
+                           <a href={`#${e.id}`}>
+                           { e.name }</a>
                            <br/>
                            { e.bio }
                        </li>
-                   })
-               }
+                })}
             </ul>
         )
     }
